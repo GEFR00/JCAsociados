333cb5f62618042b2f8a669eb40a72fd3705570a
GEFR00 <elizabehtriquelme@gmail.com>
2022-11-19 14:25:36 -0300

    creación rutas de todas las pestañas

--- webPage/src/app/app-routing.module.ts
+++ webPage/src/app/app-routing.module.ts
@@ -1,12 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AccountingComponent } from './pages/accounting/accounting.component';
+import { RemunerationsComponent } from './pages/remunerations/remunerations.component';
+import { BusinessComponent } from './pages/business/business.component';
+import { TaxesComponent } from './pages/taxes/taxes.component';
+import { ManagementComponent } from './pages/management/management.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
-  }
+  },
+  {
+    path: 'contabilidad-pymes',
+    component: AccountingComponent
+  },
+  {
+    path: 'remuneraciones',
+    component: RemunerationsComponent
+  },
+  {
+    path: 'creacion-empresas',
+    component: BusinessComponent
+  },
+  {
+    path: 'impuestos',
+    component: TaxesComponent
+  },
+  {
+    path: 'control-gestion',
+    component: ManagementComponent
+  },
 ];
 
 @NgModule({
