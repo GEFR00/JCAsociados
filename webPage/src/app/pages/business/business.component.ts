832830293cbafc0a50c6983f244c8141e7d09c0c
NicolasHidalgoGalaz <nickolashidalgo@gmail.com>
2023-01-06 18:06:15 -0300

    solicitar servicio update

--- webPage/src/app/pages/business/business.component.ts
+++ webPage/src/app/pages/business/business.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./business.component.css']
 })
 export class BusinessComponent implements OnInit {
+  public btnSolicitar: any;  
+  public form: any;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  dirigeForm() {
+    
+    this.btnSolicitar = document.querySelector('#btn-home-service') as HTMLButtonElement;
+    this.form = document.querySelector('#app-form')  as HTMLDivElement;
+
+    if( this.btnSolicitar ) {
+      this.btnSolicitar.addEventListener('click', (e: any) => {
+        this.form.scrollIntoView(true)
+      })
+    }
+    
   }
 
 }
--- webPage/src/app/pages/taxes/taxes.component.ts
+++ webPage/src/app/pages/taxes/taxes.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./taxes.component.css']
 })
 export class TaxesComponent implements OnInit {
+  public btnSolicitar: any;  
+  public form: any;
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  dirigeForm() {
+    
+    this.btnSolicitar = document.querySelector('#btn-home-service') as HTMLButtonElement;
+    this.form = document.querySelector('#app-form')  as HTMLDivElement;
+
+    if( this.btnSolicitar ) {
+      this.btnSolicitar.addEventListener('click', (e: any) => {
+        this.form.scrollIntoView(true)
+      })
+    }
+    
   }
 
 }
